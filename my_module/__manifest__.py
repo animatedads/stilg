0dfcaf35603c9cbe89e8440f043775664093b31d
Jürgen Klopp <flicker@anduin.net>
2023-09-18 09:50:34 +0100

    don't be a git

--- my_module/__manifest__.py
+++ my_module/__manifest__.py
@@ -1,15 +1,20 @@
+{# __manifest__.py
+
 {
-    'name': 'My Test Module',
-    'version': '16.0',
-    'summary': 'Short module summary',
-    'description': 'Detailed Test module description',
-    'category': 'Uncategorized',
+    'name': 'Portal Product Creation',
+    'version': '1.0',
+    'summary': 'Allows portal users to create products on the Odoo online shop',
+    'description': 'This module enables portal users to add new products with relevant details.',
+    'category': 'Website',
     'author': 'Thomas Dyer',
-    'depends': ['base'],  # Add dependencies if needed
+    'website': 'https://www.example.com',
+    'depends': ['website_sale'],
     'data': [
         'security/ir.model.access.csv',
-        'views/my_model_views.xml',
+        'views/product_views.xml',
+        'views/templates.xml',
     ],
     'installable': True,
     'application': True,
+    'auto_install': False,
 }
--- my_module/models/my_model.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class MyModel(models.Model):
-    _name = 'my.module.model'
-    _description = 'My Model'
-
-    name = fields.Char('Name', required=True)
-    description = fields.Text('Description')
